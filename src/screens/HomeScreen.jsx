c5a164f8ee127153063d36c3810e18eb1bd83c65
Gary HL <garyhl321@gmail.com>
2023-09-27 12:42:24 +0200

    Home screen vacío

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ScreenBase } from "./ScreenBase";
+import Onboarding from "../components/onboarding/Onboarding";
+
+const HomeScreen = () => {
+  return (
+    <ScreenBase>
+      <Onboarding/>
+    </ScreenBase>
+  );
+};
+
+export default HomeScreen;
