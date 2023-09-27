e739f973a251ddd7b67f52b9b390cffb70c06382
Gary HL <garyhl321@gmail.com>
2023-09-27 12:39:17 +0200

    Input de texto estilizado creado

---
+++ src/components/common/inputs/UiTextInput.jsx
@@ -0,0 +1,44 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+
+const UiTextInput = ({ style, placeholderText, ...restOfProps }) => {
+  const containerInputStyles = [
+    styles.containerInput,
+    style,
+  ];
+
+  return (
+    <View style={containerInputStyles}>
+      <Text style={styles.placeholderText}>{placeholderText}</Text>
+      <TextInput
+        style={styles.input}
+        placeholderTextColor={"gray"}
+        placeholder=""
+        {...restOfProps}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    color: "white",
+  },
+  containerInput: {
+    borderRadius: 99,
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingHorizontal: 24,
+    paddingTop:18,
+    paddingBottom:10,
+  },
+  placeholderText: {
+    color: "gray",
+    position: "absolute",
+    marginHorizontal: 24,
+    marginVertical: 4,
+    fontSize: 12,
+  },
+});
+
+export default UiTextInput;
