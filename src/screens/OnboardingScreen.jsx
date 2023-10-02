accff85bf1e4028dfa01aa3fda0d509fd2328b5a
Gary HL <garyhl321@gmail.com>
2023-10-02 10:05:12 +0200

    Creando la pantalla onboarding

---
+++ src/screens/OnboardingScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ScreenBase } from "./ScreenBase";
+import Onboarding from "../components/onboarding/Onboarding";
+
+const OnboardingScreen = () => {
+  return (
+    <ScreenBase>
+      <Onboarding />
+    </ScreenBase>
+  );
+};
+
+export default OnboardingScreen;
