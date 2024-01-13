54c1d853b4a4b81773e189a80ffe2854ac63453e
Gary HL <garyhl321@gmail.com>
2024-01-13 19:50:44 +0100

    Add new screens and components for user settings

--- src/navigation/MainStack.jsx
+++ src/navigation/MainStack.jsx
@@ -17,6 +17,13 @@ import PaymentReserveScreen from "../screens/reserves/PaymentReserveScreen";
 import MapScreen from "../screens/MapScreen";
 import YourLocation from "../screens/reserves/YourLocation";
 import ServiceDetails from "../screens/reserves/ServiceDetails";
+import RequestDetails from "../screens/reserves/RequestDetails";
+import RequestDetailsQuick from "../screens/reserves/RequestDetailsQuick";
+import DeviceSetings from "../screens/userSettings/DeviceSetings";
+import MyCards from "../screens/userSettings/MyCards";
+import MyCoupons from "../screens/userSettings/MyCoupons";
+import MyData from "../screens/userSettings/MyData";
+import PreferredLocations from "../screens/userSettings/PreferredLocations";
 
 const Stack = createStackNavigator();
 
@@ -59,6 +66,9 @@ export default function MainStack() {
         <Stack.Screen name="PaymentReserve" component={PaymentReserveScreen} />
         <Stack.Screen name="YourLocation" component={YourLocation} />
         <Stack.Screen name="ServiceDetails" component={ServiceDetails} />
+        <Stack.Screen name="RequestDetailsQuick" component={RequestDetailsQuick} />
+        <Stack.Screen name="RequestDetails" component={RequestDetails} />
+        
         {/* End-Reserves */}
 
         {/* //Auth */}
@@ -71,6 +81,15 @@ export default function MainStack() {
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
+
+        {/* User Settings */}
+        
+        <Stack.Screen name="DeviceSettins" component={DeviceSetings} />
+        <Stack.Screen name="MyCards" component={MyCards} />
+        <Stack.Screen name="MyCoupons" component={MyCoupons} />
+        <Stack.Screen name="MyData" component={MyData} />
+        <Stack.Screen name="PreferredLocations" component={PreferredLocations} />
+        {/* End User Settings */}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/userSettings/MyCoupons.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { ScreenBase } from '../screenTemplates/ScreenBase'
+import BackButton from '../../components/common/buttons/BackButton'
+
+export default function MyCoupons() {
+  return (
+    <ScreenBase dark>
+        <BackButton/>
+        <View className="mt-8">
+            
+        </View>
+
+    </ScreenBase>
+  )
+}
